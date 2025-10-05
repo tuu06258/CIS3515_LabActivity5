7c10d9b267c0d763b78ad8782c3b5169a505309f
tuu06258 <tuu06258@temple.edu>
2025-10-05 16:20:37 -0400

    Bugfix Done

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.namelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         val nameTextView = findViewById<TextView>(R.id.textView)
+        val deleteButton = findViewById<Button>(R.id.deleteButton)
 
         with (spinner) {
             adapter = CustomAdapter(names, this@MainActivity)
@@ -28,17 +30,29 @@ class MainActivity : AppCompatActivity() {
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                     p0?.run {
                         nameTextView.text = getItemAtPosition(p2).toString()
+                        deleteButton.isEnabled = names.isNotEmpty()
                     }
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
+                    nameTextView.text = ""
+                    deleteButton.isEnabled = false
                 }
             }
         }
 
-        findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
+        deleteButton.setOnClickListener {
+            val selectedIdx = spinner.selectedItemPosition
+            //Log.d("sel Idx = ", selectedIdx.toString())
+            (names as MutableList).removeAt(selectedIdx)
             (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+            //val newIdx = spinner.selectedItemPosition
+            //Log.d("new Idx = ", newIdx.toString())
+            if (names.isNotEmpty()) {
+                nameTextView.text = names[selectedIdx.coerceIn(0, names.size-1)]
+            } else {
+                nameTextView.text = ""
+            }
         }
 
     }
